8a68792e853e031dab8dffd0215987d4b8c24361
Raymond T. Hightower <rth@wisdomgroup.com>
2013-05-01 20:14:33 -0700

    updated store_config with RTH info

--- store_config.rb
+++ store_config.rb
@@ -1,13 +1,13 @@
 class StoreConfig
   def self.development_url
-    "http://localhost:3000"
+    "http://localhost:5000"
   end
 
   def self.production_url
-    "http://sample-store-demo.herokuapp.com"
+    "http://rth-portland.herokuapp.com"
   end
 
   def self.email_address
-    "katrina@jumpstartlab.com"
+    "rth@wisdomgroup.com"
   end
 end
